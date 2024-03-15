d5502a799f228da81c4fe46e21c316757d8f6148
sebastiantorreslab <str9371@gmail.com>
2024-03-15 11:38:26 -0500

    post method in golang

---
+++ post/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type Login struct {
+	Email    string `json:"email" binding:"required"`
+	Password string `json:"password" binding:"required`
+}
+
+func main() {
+
+	r := gin.Default()
+
+	r.POST("/login", func(c *gin.Context){
+		var login Login
+		c.BindJSON(&login)
+		c.JSON(200,gin.H{
+			"status":login.Email,
+		})
+	})
+
+	r.Run(":8080")
+
+
+
+}
